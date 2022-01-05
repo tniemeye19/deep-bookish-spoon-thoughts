d08d1acff925d8d31ba87f962fcab3f8e317ecf0
tniemeye19 <timothy.niemeyer19@gmail.com>
2022-01-04 21:09:09 -0500

    implement auth middleware to populate me query

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,6 +4,18 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({})
+                    .select('-__v -password')
+                    .populate('thoughts')
+                    .populate('friends');
+
+                return userData;
+            }
+
+            throw new AuthenticationError('Not logged in');
+        },
         thoughts: async (parent, { username }) => {
             const params = username ? { username } : {};
             return Thought.find().sort({ createdAt: -1 });
--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
 
+const { authMiddleware } = require('./utils/auth');
+
 // import our typedefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
@@ -14,6 +16,7 @@ const startServer = async () => {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
+    context: authMiddleware
   });
 
   // Start the Apollo server
