159b784f5aa0581b5b7edcd6fefc9190a30fa90a
tniemeye19 <timothy.niemeyer19@gmail.com>
2022-01-06 17:57:39 -0500

    add break preventer so profile page loads if user is not logged in

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
-import { useParams } from 'react-router-dom';
+import { Redirect, useParams } from 'react-router-dom';
 
 import ThoughtList from '../components/ThoughtList';
 
 import { useQuery } from '@apollo/client';
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
 import FriendList from '../components/FriendList';
+
+import Auth from '../utils/auth';
 
 const Profile = () => {
 
@@ -22,11 +24,25 @@ const Profile = () => {
     return <div>Loading...</div>;
   }
 
+  // redirect to personal profile page if username is the logged-in user's
+  if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
+    return <Redirect to="/profile" />;
+  }
+
+  // prevents breaking of page if user navigates to /profile and is not logged in
+  if (!user?.username) {
+    return (
+      <h4>
+        You need to be logged in to see this page. Use the navigation links above to sign up or log in!
+      </h4>
+    );
+  }
+
   return (
     <div>
       <div className="flex-row mb-3">
         <h2 className="bg-dark text-secondary p-3 display-inline-block">
-          Viewing {user.username}'s profile.
+          Viewing {userParam ? `${user.username}'s` : 'your'} profile.
         </h2>
       </div>
 
